d5e7dbf042bc193330489a2217657f9dd4814378
Noel Levy <nlevy@evernote.com>
2023-05-15 19:50:44 -0700

    use PDA for games, and add python test client using anchorpy

--- docs/programs/tic-tac-toe/programs/tic-tac-toe/src/instructions/setup_game.rs
+++ docs/programs/tic-tac-toe/programs/tic-tac-toe/src/instructions/setup_game.rs
@@ -1,15 +1,16 @@
 use crate::state::game::*;
 use anchor_lang::prelude::*;
 
-pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
+pub fn setup_game(ctx: Context<SetupGame>, game_id: String, player_two: Pubkey) -> Result<()> {
     ctx.accounts
         .game
         .start([ctx.accounts.player_one.key(), player_two])
 }
 
 #[derive(Accounts)]
+#[instruction(game_id: String)]
 pub struct SetupGame<'info> {
-    #[account(init, payer = player_one, space = Game::MAXIMUM_SIZE + 8)]
+    #[account(init, payer = player_one, space = Game::MAXIMUM_SIZE + 8, seeds = [game_id.as_bytes()], bump)]
     pub game: Account<'info, Game>,
     #[account(mut)]
     pub player_one: Signer<'info>,
--- docs/programs/tic-tac-toe/programs/tic-tac-toe/src/lib.rs
+++ docs/programs/tic-tac-toe/programs/tic-tac-toe/src/lib.rs
@@ -7,14 +7,14 @@ pub mod instructions;
 pub mod state;
 
 // this key needs to be changed to whatever public key is returned by "anchor keys list"
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("82iBFv96TfdP11584tHdMzw8kCqCjuak5xMkLa5yWwTd");
 
 #[program]
 pub mod tic_tac_toe {
     use super::*;
 
-    pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
-        instructions::setup_game::setup_game(ctx, player_two)
+    pub fn setup_game(ctx: Context<SetupGame>, game_id: String, player_two: Pubkey) -> Result<()> {
+        instructions::setup_game::setup_game(ctx, game_id, player_two)
     }
 
     pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
